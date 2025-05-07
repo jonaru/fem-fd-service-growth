fa5c61e984047d84ce572ca427f3a99f2df9a967
Erik Reinert <4638629+erikreinert@users.noreply.github.com>
2025-05-06 23:01:07 -0500

    patch: minor updates

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-fem-fd-service"
+    bucket = "terraform-fem-fd-service-preview"
     key    = "terraform.tfstate"
     region = "us-west-2"
   }
